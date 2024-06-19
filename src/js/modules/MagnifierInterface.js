59c1c67b6d9c1f44af2db83d368c222dad1c5818
Tony Air <tony@twma.pro>
2024-06-19 15:05:34 +0200

    IMPR: Select better voice

--- src/js/modules/MagnifierInterface.js
+++ src/js/modules/MagnifierInterface.js
@@ -1,54 +1,54 @@
 import HTMLMagnifier from 'html-magnifier/html-magnifier'
 
 class MagnifierInterface {
-    active = false
-    static #magnifier
-    static #hidden = true
+  active = false
+  static #magnifier
+  static #hidden = true
 
-    constructor() {
-        if (this.active) {
-            return this
-        }
-
-        MagnifierInterface.#magnifier = new HTMLMagnifier({
-            zoom: 2,
-            shape: 'square',
-            width: 304,
-            height: 171,
-        })
-
-        MagnifierInterface.#magnifier.on('syncScrollBars', (magnifierContent) => {
-            magnifierContent.parentElement.scrollTop = document.scrollingElement.scrollTop
-            magnifierContent.parentElement.scrollLeft = document.scrollingElement.scrollLeft
-        })
-
-
-        this.active = true
+  constructor() {
+    if (this.active) {
+      return this
     }
 
-    destroy() {
-        this.active = false
-        document.querySelectorAll('.magnifier').forEach((el) => {
-            el.remove()
-        })
+    MagnifierInterface.#magnifier = new HTMLMagnifier({
+      zoom: 2,
+      shape: 'square',
+      width: 304,
+      height: 171,
+    })
+
+    MagnifierInterface.#magnifier.on('syncScrollBars', (magnifierContent) => {
+      magnifierContent.parentElement.scrollTop = document.scrollingElement.scrollTop
+      magnifierContent.parentElement.scrollLeft = document.scrollingElement.scrollLeft
+    })
+
+
+    this.active = true
+  }
+
+  destroy() {
+    this.active = false
+    document.querySelectorAll('.magnifier').forEach((el) => {
+      el.remove()
+    })
+  }
+
+  static toggle() {
+    if (MagnifierInterface.#hidden) {
+      MagnifierInterface.show()
+      return
     }
 
-    static toggle() {
-        if (MagnifierInterface.#hidden) {
-            MagnifierInterface.show()
-            return
-        }
+    MagnifierInterface.hide()
+  }
 
-        MagnifierInterface.hide()
-    }
+  static show() {
+    MagnifierInterface.#magnifier.show()
+  }
 
-    static show() {
-        MagnifierInterface.#magnifier.show()
-    }
-
-    static hide() {
-        MagnifierInterface.#magnifier.hide()
-    }
+  static hide() {
+    MagnifierInterface.#magnifier.hide()
+  }
 }
 
 export default MagnifierInterface
--- src/js/modules/SpeechInterface.js
+++ src/js/modules/SpeechInterface.js
@@ -1,9 +1,32 @@
 class SpeechInterface {
   active = false
+  static #voice
 
   constructor() {
     if (this.active) {
       return this
+    }
+
+    const voices = window.speechSynthesis.getVoices()
+    const lang = document.documentElement.lang.slice(0, 2)
+    const sex = 'fem'
+    let langNum = 0
+
+    const availableVoices = []
+
+    voices.forEach((el) => {
+      if (el.lang.includes(lang) && el.name.includes(sex)) {
+        availableVoices.push(el)
+      }
+    })
+
+    if (lang === 'en' && availableVoices.length > 2) {
+      langNum = 1
+    }
+
+    if (typeof availableVoices[langNum] !== 'undefined') {
+      SpeechInterface.#voice = availableVoices[langNum]
+      console.log(`Found voice: ${availableVoices[langNum].name}`)
     }
 
     document.addEventListener('click', SpeechInterface.speakEvent)
@@ -23,7 +46,15 @@ class SpeechInterface {
 
   static speak(text) {
     window.speechSynthesis.cancel()
-    window.speechSynthesis.speak(new SpeechSynthesisUtterance(text))
+
+    const utr = new SpeechSynthesisUtterance(text)
+    utr.rate = 1
+    if (SpeechInterface.#voice) {
+      utr.voice = SpeechInterface.#voice
+    }
+
+
+    window.speechSynthesis.speak(utr)
   }
 }
 
